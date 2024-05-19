e5b86d771b29162719dcc76a97a00efe572992f2
김태우(RXD) <113758615+cher1shRXD@users.noreply.github.com>
2024-05-19 15:59:49 +0900

    Delete successLife directory

--- successLife/backend/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-@session_start();
-session_destroy();
-echo "<script>alert(\"로그아웃 되었습니다.\"); location.href='../main';</script>";
-?>
--- successLife/js/chk_upload.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("prob_form").onsubmit=function(){
-    var confirm_chk= confirm("출제하시겠습니까? 출제 후에는 문제를 수정할 수 없습니다.");
-    if (!confirm_chk) {
-        return false;
-    }
-}
